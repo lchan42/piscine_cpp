5b751c1afbf9aa461bc63ab500ef11f4a346c5af
lchan <lchan@student.42.fr>
2022-10-22 14:30:19 +0200

    ouais c'est vrai

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -5,72 +5,74 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lchan <lchan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/20 19:33:25 by lchan             #+#    #+#             */
-/*   Updated: 2022/10/20 20:28:25 by lchan            ###   ########.fr       */
+/*   Created: 2022/10/22 13:27:56 by lchan             #+#    #+#             */
+/*   Updated: 2022/10/22 14:14:55 by lchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
+#include <iostream>
+#include <exception>
 
-# include <iostream>
-
-template<typename T>
+template <typename T>
 class Array{
-	public :
-
-		Array<T>() : _tab(0), _size(0)
-		{
-			std::cout << "[Array] default destructor called" << std::endl;
-		}
-
-		Array<T>(unsigned int n) : _size (n)
-		{
-			std::cout << "[Array] param destructor called" << std::endl;
-			if (n <= 0)
-				throw std::overflow_error("invalid size\n");
-			_tab = new T[n];
-
-		}
-
-		Array<T>(const Array &cpy)
-		{
-			*this = cpy;
-		}
-
-		~Array<T>()
-		{
-			if (_tab)
-				delete[] (_tab);
-		}
-
-		Array<T> &operator=(const Array <T> &rhs){
-
-			if (this == &rhs)
-				return *this;
-			if (_tab)
-				delete[] (this->_tab);
-			this->_size = rhs.size();
-			this->_tab = new T[this->_size];
-			for (int i = 0; i < this->_size; i++)
-				this->_tab[i] = rhs[i];
-			return (*this);
-		}
-
-		T & operator[](const int index) const
-		{
-			if (index >= this->_size)
-				throw std::overflow_error("invalid size\n");
-			return(this->_tab[index]);
-		}
-
-		int	size() const{
-			return (_size);
-		}
 	private:
 		T	*_tab;
 		int	_size;
 
-};
+	public:
+		Array<T>() : _tab(0), _size(0){
 
-#endif
+			std::cout << "[Array] default constructor called" << std::endl;
+		}
+
+		Array<T>(Array const & cpy){
+
+			*this = cpy;
+			std::cout << "[Array] cpy constructor called" << std::endl;
+		}
+
+		Array<T>(unsigned int n) : _size(n){
+
+			if (n <= 0 )
+				throw std::overflow_error("invalid size\n");
+			_tab = new T [n];
+			std::cout << "[Array] param constructor called" << std::endl;
+		}
+
+		~Array<T>(){
+
+			if (this->_tab)
+				delete [] (this->_tab);
+			std::cout << "[Array] cpy destructor called" << std::endl;
+
+		}
+
+		Array<T> & operator=(Array const & rhs){
+
+			if (&rhs == this)
+				return(*this);
+			if (this->_tab)
+				delete[] (this->_tab);
+			this->_size = rhs.size();
+			if (rhs._tab)
+			{
+				this->_tab = new T [this->_size];
+				for(int i = 0; i < _size; i++)
+					this->_tab[i] = rhs[i];
+			}
+			else
+				this->_tab = rhs._tab;
+			return(*this);
+		}
+
+		T & operator[](int const index) const{
+
+			if(index >= _size)
+				throw std::overflow_error("overflow_error");
+			return (_tab[index]);
+		}
+		int	size() const{
+
+			return (_size);
+		}
+};
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,17 +6,18 @@
 /*   By: lchan <lchan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:53:41 by lchan             #+#    #+#             */
-/*   Updated: 2022/10/20 20:27:26 by lchan            ###   ########.fr       */
+/*   Updated: 2022/10/22 14:26:51 by lchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int	main( void )
+
+int	main(void)
 {
-	Array<int>			intArray;
-	Array<std::string>	stringArray(10);
-	Array<std::string>	otherStringArray;
+	Array<int>			intArray(6);
+	Array<std::string>	stringArray(6);
+	Array<std::string>	stringArray2;
 
 	stringArray[0] = "Hi";
 	stringArray[1] = "Hello";
@@ -24,28 +25,90 @@ int	main( void )
 	stringArray[3] = "Fine and you?";
 	stringArray[4] = "Doing great thanks!";
 	stringArray[5] = "You're very welcome";
-	std::cout << "intArray.size() = " << intArray.size() << std::endl;
-	std::cout << "stringArray.size() = " << stringArray.size() << std::endl;
-	otherStringArray = stringArray;
-	otherStringArray[3] = "I'm good, how about you?";
 
-	try
-	{
-		std::cout << stringArray[59] << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	intArray[0] = 42;
+	intArray[1] = 422;
+	intArray[2] = 4222;
+	intArray[3] = 42222;
+	intArray[4] = 422222;
+	intArray[5] = 4222222;
 
-	try
+
+	for(int i = 0; i < 10; i++)
 	{
-		std::cout << stringArray[-3] << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
+		try {
+			std::cout << "stringArray[" << i << "] = "<< stringArray[i] << std::endl;
+
+		}
+		catch (std::exception & e){
+			std::cout << e.what() << std::endl;
+		}
 	}
 
-	return 0;
+	stringArray2 = stringArray;
+	for(int i = 0; i < 10; i++)
+	{
+		try {
+			std::cout << "stringArray2[" << i << "] = "<< stringArray2[i] << std::endl;
+		}
+		catch (std::exception & e){
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+		try {
+			std::cout << "intArray[" << i << "] = "<< intArray[i] << std::endl;
+
+		}
+		catch (std::exception & e){
+			std::cout << e.what() << std::endl;
+		}
+	}
+	//stringArray = intArray; not accepted by compiler
 }
+
+
+
+
+
+
+
+// int	main( void )
+// {
+// 	Array<int>			intArray;
+// 	Array<std::string>	stringArray(10);
+// 	Array<std::string>	otherStringArray;
+
+// 	stringArray[0] = "Hi";
+// 	stringArray[1] = "Hello";
+// 	stringArray[2] = "How are you?";
+// 	stringArray[3] = "Fine and you?";
+// 	stringArray[4] = "Doing great thanks!";
+// 	stringArray[5] = "You're very welcome";
+// 	std::cout << "intArray.size() = " << intArray.size() << std::endl;
+// 	std::cout << "stringArray.size() = " << stringArray.size() << std::endl;
+// 	otherStringArray = stringArray;
+// 	otherStringArray[3] = "I'm good, how about you?";
+
+// 	try
+// 	{
+// 		std::cout << stringArray[59] << std::endl;
+// 	}
+// 	catch (std::exception &e)
+// 	{
+// 		std::cout << e.what() << std::endl;
+// 	}
+
+// 	try
+// 	{
+// 		std::cout << stringArray[-3] << std::endl;
+// 	}
+// 	catch (std::exception &e)
+// 	{
+// 		std::cout << e.what() << std::endl;
+// 	}
+
+// 	return 0;
+// }
